a25307ea8ec3f5f7b637e65737f05ba0d28c0f80
Ahsaan Ansari <ahsaan.se15@gmail.com>
2020-01-08 12:17:39 +0500

    Modified Data

--- app/src/main/java/com/example/androidrestfulapi/utils/NetworkHelper.java
+++ app/src/main/java/com/example/androidrestfulapi/utils/NetworkHelper.java
@@ -1,4 +1,20 @@
 package com.example.androidrestfulapi.utils;
 
-public class NetworkHelper {
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+public class NetworkHelper{
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public static boolean isNetworkAvailable(Context context){
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE   );
+
+        NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+
+        return info != null && info.isConnectedOrConnecting();
+
+    }
 }
